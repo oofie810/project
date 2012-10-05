4a89a5b84b73b8c312b2707a939d1432b3d38218
Ronald Ongjoco <ron.ongjoco@gmail.com>
2012-10-04 19:13:35 -0700

    LogAction class

--- lib/LogAction.php
+++ lib/LogAction.php
@@ -1,14 +1,14 @@
 <?php
-    class LogAction{
-	public static function insertLog($userId, $action){
-	
+  class LogAction{
+	  public static function insertLog($userId, $action){
 	    $ip = $_SERVER['REMOTE_ADDR'];
 	    $sql = 'INSERT INTO action_log (user_id, action_id, date, ip_address) VALUES (:id, :action, NOW(), :ip)';
 	    $params = array (':id'	=> $userId,
-			     ':action'	=> $action,
-			     ':ip'	=> $ip);
+			                 ':action'	=> $action,
+			                 ':ip'	=> $ip);
 	    Database::insert ($sql, $params);
-	}
-    }
+	  }
+  
+  }
 ?>
 
