a4c013ab629d35151f34a12ef8944ae6ebb9882c
Ronald Ongjoco <ron.ongjoco@gmail.com>
2013-03-10 16:27:01 -0700

    fixed sidebar2 with working links

--- pages/sidebar2.php
+++ pages/sidebar2.php
@@ -1,22 +1,17 @@
 <div id="sidebar">
 <ul class="menu">
-  <li><span><a href="#_" onclick="alert('Coming soon!'); return false;">Featured Recipes</a></span>
-    <ul>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Chicken Teriyaki</a></li>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Baked Salmon</a></li>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Tiramisu</a></li>
-    </ul>
-  </li>
   <li><span><a href="viewset.php?category=5">Entrees</a></span>
     <ul>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Beef</a></li>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Pork</a></li>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Chicken</a></li>
-      <li><a href="#_" onclick="alert('Coming soon!'); return false;">Fish</a></li>
+      <li><a href="search.php?search_box=beef">Beef</a></li>
+      <li><a href="search.php?search_box=Chicken">Chicken</a></li>
+      <li><a href="search.php?search_box=pork">Pork</a></li>
+      <li><a href="search.php?search_box=fish+seafood">Fish / Seafood</a></li>
     </ul>
   </li>
   <li><span><a href="viewset.php?category=1;">Appetizers</a></span></li>
   <li><span><a href="viewset.php?category=3">Salads</a></span></li>
   <li><span><a href="viewset.php?category=6">Pasta</a></span></li>
   <li><span><a href="viewset.php?category=2;">Desserts</a></span></li>
+  <li><span><a href="viewset.php?category=7;">Pastas</a></span></li>
+  <li><span><a href="viewset.php?category=8;">Breakfast</a></span></li>
 </div>
